a59872d1cc8ff6935a4c56946f3ac4d67a3b0a55
Lucciano Perticarini <lucciperti7@gmail.com>
2026-05-04 21:54:50 -0300

    feat(app): added logic and validation of payday

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-    import { ref } from 'vue';
+    import { computed, ref } from 'vue';
 
     type DiaLaboral = 'Lunes' | 'Martes' | 'Miercoles' | 'Jueves' | 'Viernes';
 
@@ -20,6 +20,15 @@
         'Jueves':   ['6', '7'],
         'Viernes': ['8','9']
     }
+
+    const correspondeCobro = computed(() =>{
+        if (dniInput.value.length === 0 ) return null
+
+        const ultimoDigito = dniInput.value.slice(-1)
+        const digitosDelDia =cronograma[diaSeleccionado.value]
+
+        return digitosDelDia.includes(ultimoDigito)
+    })
 </script>
 
 <template>
@@ -37,15 +46,19 @@
             </button>
         </div>
         <div class="w-full max-w-md bg-white p-6 rounded-2xl shadow-sm border border-slate-200 mt-6">
-        <label class="block text-sm font-semibold text-slate-700 mb-2">Ingrese su DNI</label>
-        <input
-            v-model="dniInput"
-            type="text"
-            placeholder="Ej: 40123456"
-            maxlength="8"
-            class="w-full p-3 border border-slate-300 rounded-xl foucs:ring-2 focus:ring-indigo-500 outline-none transition-all"
-        />
-    </div>
+            <label class="block text-sm font-semibold text-slate-700 mb-2">Ingrese su DNI</label>
+            <input
+                v-model="dniInput"
+                type="text"
+                placeholder="Ej: 40123456"
+                maxlength="8"
+                class="w-full p-3 border border-slate-300 rounded-xl foucs:ring-2 focus:ring-indigo-500 outline-none transition-all"
+            />
+        </div>
+        <div v-if="dniInput" class="mt-6">
+            <p v-if="correspondeCobro" class="text-green-600 font-bold text-lg">¡Hoy te corresponde el pago!</p>
+            <p v-else class="text-red-500">Hoy no es tu turno de cobro</p>
+        </div>
     </div>
     
 </template>
